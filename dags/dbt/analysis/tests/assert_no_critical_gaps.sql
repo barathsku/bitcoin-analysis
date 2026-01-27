7b6c3b4cd644a3c6e1e0ac634135ac953fc08c4e
barathsku <barathsku@gmail.com>
2026-01-27 23:30:35 +0800

    Add custom test to check for critical data gaps

--- dags/dbt/analysis/tests/assert_no_critical_gaps.sql
+++ dags/dbt/analysis/tests/assert_no_critical_gaps.sql
@@ -1,12 +1,14 @@
 {#
-  Singular test: Fails if there are unexpected data gaps for critical assets.
+  Throws error if there are unexpected data gaps for critical assets.
   
   - BTC (crypto) should have no gaps (trades 24/7)
   - Stocks should have no gaps on trading days
   - Forex gaps on weekends are acceptable, hence excluded
   
-  This test will fail the dbt run/build if any rows are returned.
+  This test will throw an error if any rows are returned.
 #}
+
+{{ config(severity='error') }}
 
 select
     asset_id,
@@ -17,7 +19,7 @@ from {{ ref('int_data_gaps') }}
 where 
     -- BTC should have no gaps at all
     asset_type = 'crypto'
-    -- Stocks should have no gaps on trading days (Mon-Fri)
+    -- Stocks should have no gaps on trading days (Mon-Fri) excluding holidays
     or (asset_type in ('stock', 'index') and is_trading_day = 1)
 order by asset_id, missing_date
 limit 100  -- Show first 100 gaps if test fails
