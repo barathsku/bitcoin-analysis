3f71947687e37da1ef33d6b1873a2f2c241e4459
barathsku <barathsku@gmail.com>
2026-01-28 23:25:52 +0800

    Add preliminary correlation report

---
+++ dags/dbt/analysis/models/reports/rpt_correlation.sql
@@ -0,0 +1,27 @@
+{{ config(location=generate_external_location()) }}
+
+with daily_returns as (
+    select
+        data_date,
+        asset_id,
+        asset_name,
+        daily_return
+    from {{ ref('fct_daily_returns') }}
+    where daily_return is not null
+),
+
+correlations as (
+    select
+        a.asset_id as asset_a,
+        a.asset_name as asset_a_name,
+        b.asset_id as asset_b,
+        b.asset_name as asset_b_name,
+        corr(a.daily_return, b.daily_return) as correlation_coefficient,
+        count(*) as observation_count
+    from daily_returns a
+    join daily_returns b on a.data_date = b.data_date
+    group by 1, 2, 3, 4
+)
+
+select * from correlations
+order by asset_a, correlation_coefficient desc
